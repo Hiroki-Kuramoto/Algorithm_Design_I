865a480e0e10dfef333d70b77c82df21ef4b3446
Hiroki Kuramoto <kuramoto.hiroki.359@s.kyushu-u.ac.jp>
2022-04-26 13:24:07 +0900

    表示の仕様をdpと統一化

--- exhaustiveSearch.py
+++ exhaustiveSearch.py
@@ -15,12 +15,12 @@ for i in range(0,2**len(w)):
     # decimal 5 -> binary 011
     # In this case, target means [NotUse,Use,Use].
     maxValue = 0
-    Solution = []
+    Solution = "*"
     target = str(bin(i+2**len(w))).replace("0b1","")
     for j,flag in enumerate(target):
         if flag == "1":
             maxValue += w[j]
-            Solution.append(str(j+1))
+            Solution += f"_{j+1}"
     if W >= maxValue:
         if maxValue > optimalValue:
             optimalSolution = [Solution]
@@ -28,5 +28,6 @@ for i in range(0,2**len(w)):
         elif maxValue == optimalValue:
             optimalSolution.append(Solution)
 
+optimalSolution.reverse()
 print(f"optimalValue {optimalValue}")
 print(f"optimalSolution {optimalSolution}")
