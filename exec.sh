d2ca303cba1d097ac3bf6bdc2bf16e808a032ae7
Hiroki Kuramoto <kuramoto.hiroki.359@s.kyushu-u.ac.jp>
2022-04-27 10:24:49 +0900

    表示を変更

--- exec.sh
+++ exec.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+echo ----- start tableMaker -----
 time python tableMaker.py
-
+echo ----- start exhaustiveSearch -----
 time python exhaustiveSearch.py
-
+echo ----- start dpSearch -----
 time python dpSearch.py
